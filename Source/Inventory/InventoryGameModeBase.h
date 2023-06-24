975e50475862a04b39d96d3db01748759e8c053b
Michele Rossi <m.rossi@overside.it>
2023-06-24 19:22:23 +0200

    first commit

---
+++ Source/Inventory/InventoryGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "InventoryGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INVENTORY_API AInventoryGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
